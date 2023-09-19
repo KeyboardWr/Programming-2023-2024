c3269d0fabc48eb1cab4aff68264f269f5c75e4b
Herman Li <Hermanli2021@gmail.com>
2023-09-19 11:19:25 -0700

    Added notes and python file

---
+++ Notes/quote_generator.py
@@ -0,0 +1,17 @@
+#quote Generator
+#Author: Herman
+#September 19
+
+# Output a quote fro someone famous
+print ("Due dates are closer than it seems ---some poster")
+
+print ("With great power comes great responsiblity ---Ben Parker")
+
+print("Ok ---Rammus")
+
+
+#Ask the user a question
+input("What was your favourite quote?")
+
+# Respond
+print("Oh thats my favourite too")
