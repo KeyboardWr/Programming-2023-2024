e1cb2d70f46952b6dd339afb9283cace250121fe
keyboardWr <whobbd@gmail.com>
2023-11-04 23:44:31 -0700

    Updated age in 2049 bot

--- age_in_2049.py
+++ age_in_2049.py
@@ -6,9 +6,9 @@
 
 
 User_Current_Age = int(input("Hi, what is your age currently?"))
+
+
 # Add 26 years to their response
-
-
 Age_In_2049 = User_Current_Age + 26
 print(f"You will be {Age_In_2049} in 2049")
 
