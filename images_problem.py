55425a1c3f90c4bc71d29a5d88745582fabc93cf
Herman Li <Hermanli2021@gmail.com>
2023-12-18 11:11:43 -0800

    images

---
+++ images_problem.py
@@ -0,0 +1,18 @@
+# Images Problem
+# Herman Li
+# December 18 2023
+
+def is_light(pixel: tuple) -> bool:
+    """
+    """
+
+
+    return
+
+# Test?
+light_pixel = (255, 255, 255)
+light_gray = (128.128,128)
+dark_grey = (127,127,127)
+dark_piel = (0,0,0)
+
+print(is_light(light_pixel)) # True
